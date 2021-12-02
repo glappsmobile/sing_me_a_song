1fddff89e90d9d72fe26a0be10da3d06f14a90a9
Glauco Villas Boas <glappsmobile@gmail.com>
2021-12-02 12:30:49 -0300

    fixed repository query

--- src/repositories/genre.repository.js
+++ src/repositories/genre.repository.js
@@ -2,7 +2,7 @@ import connection from '../database/connection.js';
 
 const createGenre = async ({ name }) => {
   const transactionQuery = await connection.query(
-    'INSERT INTO "genres" ("naame") VALUES ($1) RETURNING name;',
+    'INSERT INTO "genres" ("name") VALUES ($1) RETURNING name;',
     [name],
   );
 
