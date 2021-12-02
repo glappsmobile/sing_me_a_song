03b9559ecc8fc9858b589f04eb7f9d419bb0e836
Glauco Villas Boas <glappsmobile@gmail.com>
2021-12-02 12:43:25 -0300

    created error for too big names

--- src/controllers/genre.controller.js
+++ src/controllers/genre.controller.js
@@ -1,6 +1,7 @@
 import * as genreService from '../services/genre.service.js';
 import * as genreSchema from '../schemas/genre.schema.js';
 import ConflictError from '../errors/ConflictError.js';
+import BadRequestError from '../errors/BadRequestError.js';
 
 const createGenre = async (req, res, next) => {
   try {
@@ -11,11 +12,16 @@ const createGenre = async (req, res, next) => {
     const { name } = req.body;
     const genre = await genreService.createGenre({ name });
 
-    return res.send(genre).status(201);
+    return res.send(genre);
   } catch (error) {
     if (error instanceof ConflictError) {
       return res.status(404).send(error.message);
     }
+
+    if (error instanceof BadRequestError) {
+      return res.status(400).send(error.message);
+    }
+
     next(error);
   }
 };
--- src/services/genre.service.js
+++ src/services/genre.service.js
@@ -1,7 +1,12 @@
 import * as genreRepository from '../repositories/genre.repository.js';
 import ConflictError from '../errors/ConflictError.js';
+import BadRequestError from '../errors/BadRequestError.js';
 
 const createGenre = async ({ name }) => {
+  if (name.length > 255) {
+    throw new BadRequestError('Name is too big (over 255 chars).');
+  }
+
   const genre = await genreRepository.findGenreByName({ name });
 
   if (genre) {
