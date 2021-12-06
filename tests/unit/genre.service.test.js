ddab381c9ec615b8801d01239c4c5222fa553b10
Glauco Villas Boas <glappsmobile@gmail.com>
2021-12-05 22:07:38 -0300

    added tests for getGenreById service function

--- tests/unit/genre.service.test.js
+++ tests/unit/genre.service.test.js
@@ -3,12 +3,14 @@ import * as genreRepository from '../../src/repositories/genre.repository.js';
 import * as genreFactory from '../factories/genre.factory.js';
 import GenreParamsError from '../../src/errors/GenreParamsError.js'
 import GenreConflictError from '../../src/errors/GenreConflictError.js'
+import GenreNotFoundError from '../../src/errors/GenreNotFoundError.js'
 
 const sut = genreService;
 const mockGenreRepository = {
   getGenreByName: () => jest.spyOn(genreRepository, 'getGenreByName'),
   createGenre: () =>  jest.spyOn(genreRepository, 'createGenre'),
   getAllGenres: () =>  jest.spyOn(genreRepository, 'getAllGenres'),
+  getGenreById: () =>  jest.spyOn(genreRepository, 'getGenreById'),
 }
 
 describe('Genre Service', () => {
@@ -41,5 +43,45 @@ describe('Genre Service', () => {
     const result = await sut.getAllGenres();
 
     expect(result).toEqual(mockedGenres);
+  });
+});
+
+describe('Genre Service: getGenreById', () => {
+  it('Should throw a GenreNotFoundError when there are no genres with given id', async () => {
+    mockGenreRepository.getGenreById().mockImplementationOnce(() => false);
+
+    const promise = sut.getGenreById({ id: 1 });
+
+    await expect(promise).rejects.toThrowError(GenreNotFoundError);
+  });
+
+  it('Should only return id and name when the genre score is null', async () => {
+    const genre = {
+      id: 1,
+      name: "mock",
+      score: null,
+      recommendations: []
+    }
+
+    mockGenreRepository.getGenreById().mockImplementationOnce(() => genre);
+
+    const result = await sut.getGenreById({ id: 1 });
+
+    expect(result).toEqual({ id: genre.id, name: genre.name });
+  });
+
+  it('Should return the complete genre when the genre exists and the score is not null', async () => {
+    const genre = {
+      id: 1,
+      name: "mock",
+      score: 10,
+      recommendations: []
+    }
+
+    mockGenreRepository.getGenreById().mockImplementationOnce(() => genre);
+
+    const result = await sut.getGenreById({ id: 1 });
+
+    expect(result).toEqual(genre);
   });
 });
